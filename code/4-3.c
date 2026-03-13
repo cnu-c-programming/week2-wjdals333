45ba74f81e457b5a8be0dca7f30221d644a523c4
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-03-13 12:08:31 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,32 @@
 
 int main()
 {
+    int a;
+    scanf("%d", &a);
 
+    int is_prime = 1; // true면 1 false면 0
+    if (a <= 1)
+    {
+        is_prime = 0;
+    }
+    else {
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0)
+            {   
+                is_prime = 0;
+                break;
+            }
+        }
+    }
+
+    if (is_prime == 1)
+    {
+        printf("true");
+    }
+    else {
+        printf("false");
+    }
+    
     return 0;
 }
 
