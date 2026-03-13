04d7f43549db4c0b4ddc2eebcc47babe942db392
Kim Jeongmin <131476493+wjdals333@users.noreply.github.com>
2026-03-13 11:46:25 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,27 @@
 
 int main()
 {
+    int a;
+    int b;
+    char operator;
+
+    scanf("%d %d %c", &a, &b, &operator);
+    if (operator == '+')
+    {
+        printf("%d", a + b);
+    }
+    else if (operator == '-')
+    {
+        printf("%d", a - b);
+    }
+    else if (operator == '*')
+    {
+        printf("%d", a * b);
+    }
+    else if (operator == "/")
+    {
+        printf("%d", a / b);
+    }
 
     return 0;
 }
